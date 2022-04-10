7f5e138c7185c70a17aca32559354d803154b2ee
Sagar <skmalakar.1218089@gmail.com>
2022-04-10 20:44:22 +0530

    Added assignment8

---
+++ Assignment8/Reflection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionTest
+{
+    public class MainClass
+    {
+        public static void UseReflection()
+        {
+            Assembly assembly = Assembly.LoadFrom(@"C:\Users\Sagar\Desktop\parctice .NET\EmployeeManagementSystem\EmployeeManagementSystem\bin\Debug\net6.0\EmployeeManagementSystem.dll");
+
+            Type[] type = assembly.GetTypes();
+            foreach(Type t in type)
+            {
+                Console.WriteLine(t.FullName);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Displaying the types using Reflection.");
+            UseReflection();
+        }
+    }
+}
